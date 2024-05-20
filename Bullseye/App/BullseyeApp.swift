396198f99444200d32e4cc5ce08665db2723b6aa
Austin Bond <austinbond@icloud.com>
2024-05-20 22:05:42 +0300

    Project Files

---
+++ Bullseye/App/BullseyeApp.swift
@@ -0,0 +1,16 @@
+//
+//  Bullseye.swift
+//  Bullseye
+//
+//  Created by Austin Bond on 5/4/24.
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ Bullseye/Models/Game.swift
@@ -0,0 +1,61 @@
+//
+//  Game.swift
+//  Bullseye
+//
+//  Created by Austin Bond on 5/4/24.
+
+import Foundation
+
+struct Game {
+    var target = Int.random(in: 1...100)
+    var score = 0
+    var round = 1
+    var leaderboardEntries: [LeaderboardEntry] = []
+    
+    init(loadTestData: Bool = false) {
+        if loadTestData {
+            addtoLeaderboard(score: 100)
+            addtoLeaderboard(score: 80)
+            addtoLeaderboard(score: 200)
+            addtoLeaderboard(score: 50)
+            addtoLeaderboard(score: 20)
+        }
+    }
+    
+    func points(sliderValue: Int) -> Int {
+        let difference = abs(target - sliderValue)
+        let bonus: Int
+        if difference == 0 {
+            bonus = 100
+        } else if difference <= 2 {
+            bonus = 50
+        } else {
+            bonus = 0
+        }
+        return 100 - difference + bonus
+    }
+    
+    mutating func startNewRound(points: Int) {
+        addtoLeaderboard(score: points)
+        score += points
+        round += 1
+        target = Int.random(in: 1...100)
+    }
+    
+    mutating func restart() {
+        score = 0
+        round = 1
+        target = Int.random(in: 1...100)
+    }
+    
+    mutating func addtoLeaderboard(score:Int) {
+        leaderboardEntries.append(LeaderboardEntry(score: score, date: Date()))
+        leaderboardEntries.sort { entry1, entry2 in
+            entry1.score > entry2.score
+        }
+    }
+}
+    struct LeaderboardEntry {
+        let score: Int
+        let date: Date
+    }
